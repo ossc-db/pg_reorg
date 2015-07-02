d20966d49d050431390a1219f8895888a7e5dcd6
bwtakacy <bwtakacy@gmail.com>
2015-07-02 14:11:58 +0900

    Move HTML files to top and remove other files.

--- bin/sql/init-extension.sql
+++
@@ -1,5 +0,0 @@
-SET client_min_messages = warning;
-\set ECHO none
-CREATE EXTENSION pg_reorg;
-\set ECHO all
-RESET client_min_messages;
--- lib/uninstall_pg_reorg.sql
+++
@@ -1,8 +0,0 @@
-/*
- * pg_reorg: lib/uninstall_reorg.sql
- *
- * Portions Copyright (c) 2008-2015, NIPPON TELEGRAPH AND TELEPHONE CORPORATION
- * Portions Copyright (c) 2011, Itagaki Takahiro
- */
-
-DROP SCHEMA IF EXISTS reorg CASCADE;
